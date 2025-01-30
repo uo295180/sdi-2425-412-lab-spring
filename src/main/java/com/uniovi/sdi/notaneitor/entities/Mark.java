70664ac76261955c361b13cf039235c0ed20cef5
orvizz <153991700+orvizz@users.noreply.github.com>
2025-01-30 17:55:39 +0100

    SDI-2425-412-2.2-Trabajando con Controladores.

---
+++ src/main/java/com/uniovi/sdi/notaneitor/entities/Mark.java
@@ -0,0 +1,33 @@
+package com.uniovi.sdi.notaneitor.entities;
+
+public class Mark {
+    private Long id;
+    private String description;
+    private Double score;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+
+}
